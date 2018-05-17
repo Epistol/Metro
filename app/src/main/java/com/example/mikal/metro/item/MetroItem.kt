ae4a987cc127ed4d718774a7a6bb1a478851a7a8
Epistol <mika63270@live.fr>
2018-05-17 11:19:06 +0200

    metroitem

--- app/src/main/java/com/example/mikal/metro/item/MetroItem.kt
+++ app/src/main/java/com/example/mikal/metro/item/MetroItem.kt
@@ -1,11 +1,35 @@
 package com.example.mikal.metro.item
 
+import android.view.View
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.items.AbstractItem
+
 /**
  * Created by mikal on 17/05/2018.
  */
-class MetroItem {
+class MetroItem: AbstractItem<MetroItem, MetroItem.MetroViewHolder>() {
+    override fun getLayoutRes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
-    class MetroViewHolder{
+    override fun getType(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getViewHolder(v: View?): MetroViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class MetroViewHolder: FastAdapter.ViewHolder<MetroItem>{
+
+        override fun unbindView(item: MetroItem?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun bindView(item: MetroItem?, payloads: MutableList<Any>?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
 
     }
 }
