ff91734cb934c51fcbb5422863dc77ea58d499b2
Epistol <mika63270@live.fr>
2018-05-17 09:50:08 +0200

    init

--- app/src/main/java/com/example/mikal/metro/MainActivity.kt
+++ app/src/main/java/com/example/mikal/metro/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.mikal.metro
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,38 @@ class MainActivity : AppCompatActivity() {
         // Accès a une string depuis le code
         // R.string.app_name
 
-        val button = findViewById<Button>(R.id.button_login)
+        // Avec kotlin inutile depuis une activity
+//        val button = findViewById<Button>(R.id.button_login)
+
+
+        button_login.setOnClickListener {
+            val login = editText_Texte.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if(login.isNotEmpty()&&password.isNotEmpty()){
+                val alertDialog = AlertDialog.Builder(this)
+                        .setTitle(R.string.login_success_title)
+                        .setMessage(R.string.login_success_message)
+                        .setPositiveButton(android.R.string.ok,
+                                { dialogInterface: DialogInterface, buttonIndex: Int ->
+                                    //callback du clic sur le bouton Ok
+                                }).create()
+
+                alertDialog.show()
+            }
+
+//            -> Afficher le nouvel écran
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+            finish()
+
+
+
+
+        }
+
+
     }
 }
